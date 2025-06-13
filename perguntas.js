44b95fdfe9c70a1c7c484a912f992ea7ced3680d
beatrizlazzarimelo <beatriz.lazzari.melo@escola.pr.gov.br>
2025-06-13 08:47:03 -0300

    depois continua

---
+++ perguntas.js
@@ -0,0 +1,51 @@
+CriarCartao
+
+#container {
+    display: flex;
+     flex-wrap: wrap;
+     justify-content: space-between;
+     padding: 4rem;
+     gap: 3rem;
+}
+
+criaCartao(
+    'Frances',
+    'Como se diz sol em frances',
+    'soleil'
+)
+
+criaCartao(
+    'Japones',
+    'Oque significa 弓道?',
+    'O caminho do arco'
+)
+
+criaCartao(
+    'Portugues',
+    'Como se diz injeção em portugues de portugal?',
+    'pica'
+)
+
+criaCartao(
+    'italiano',
+    'como fala bom dia em italiano',
+    'Buongiorno'
+)
+
+criaCartao(
+    'Frances',
+    'Como se diz senhor em frances',
+    'monsieur'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
